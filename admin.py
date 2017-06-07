acf8a94b93ca40d189742ded1a27886c59e088c6
Kalu Kalu <1718221904@qq.com>
2017-06-07 15:30:41 +0800

    added Job Listenings model

--- admin.py
+++ admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from.models import (
+    JobListing,
+)
 
-# Register your models here.
+
+class JobListingAdmin(admin.ModelAdmin):
+    List_display = ('title', 'description')
+
+
+admin.site.register(JobListing, JobListingAdmin)
--- views.py
+++ views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
-
+from .models import JobListing
 # Create your views here.
 
 def home(request):
-    return render(request, 'jobs_app/index.html')
+
+    jobs_listings = JobListing.objects.all()
+
+    return render(request, 'jobs_app/index.html', {'jobs_listings': jobs_listings})
